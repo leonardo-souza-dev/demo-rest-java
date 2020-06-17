7115dc14ccebda7048c20ace55330af634173559
Leonardo Souza <leonardosouza@mm.local>
2020-06-17 20:29:36 -0300

    refactoring

--- src/test/java/com/example/demo/IdentityTests.java
+++ src/test/java/com/example/demo/IdentityTests.java
@@ -1,9 +1,10 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.math.BigDecimal;
 
 import com.example.demo.model.Usuario;
 import com.example.demo.service.IdentityServiceImpl;
@@ -13,13 +14,28 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 public class IdentityTests {
 
-    @Test
-    public void naoDeveCadastrarUsuarioComSenhaCurta() {
+
+    @ParameterizedTest
+    @CsvSource({ "gmail@uol.com, 123456789012356", "teste@gmail.com, 12345678" })
+    //@Test
+    public void validarSucessoDeCadastro(String email, String senha) {
 		
         // arrange
-        String email = "teste@gmail.com";
-        String senha = "123";
+        IdentityServiceImpl sut = new IdentityServiceImpl();
+        
+        // act
+        Usuario usuario = sut.cadastrarUsuario(email, senha);
 
+        // assert
+        assertNotNull(usuario);
+    }
+
+    @ParameterizedTest
+    @CsvSource({ "teste@uol.com, 123456789012356", "teste@gmail.com, 123" })
+    //@Test
+    public void validarErroDeCadastro(String email, String senha) {
+		
+        // arrange
         IdentityServiceImpl sut = new IdentityServiceImpl();
         
         // act
@@ -28,20 +44,4 @@ public class IdentityTests {
         // assert
         assertNull(usuario);
     }
-
-    // @Test
-    // public void naoDeveCadastrarQuandoDiferenteDeGmail() {
-		
-    //     // arrange
-    //     String email = "teste@teste.com";
-    //     String senha = "1";
-
-    //     IdentityServiceImpl sut = new IdentityServiceImpl();
-        
-    //     // act
-    //     Usuario usuario = sut.cadastrarUsuario(email, senha);
-
-    //     // assert
-    //     assertNull(usuario);
-    // }
 }
