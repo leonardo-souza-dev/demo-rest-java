2188de5deb26e8b267d19dff35c9ff086e95b385
Leonardo souza <leonardotreze@gmail.com>
2020-07-13 13:21:42 -0300

    adjusts

---
+++ src/test/java/com/example/demo/IdentityTests2.java
@@ -0,0 +1,47 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
+
+import com.example.demo.model.Usuario;
+import com.example.demo.service.IdentityServiceImpl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class IdentityTests2 {
+
+    @Test
+    public void naoDeveCadastrarUsuarioComSenhaCurta() {
+		
+        // arrange
+        String email = "teste@gmail.com";
+        String senha = "123";
+
+        IdentityServiceImpl sut = new IdentityServiceImpl();
+        
+        // act
+        Usuario usuario = sut.cadastrarUsuario(email, senha);
+
+        // assert
+        assertNull(usuario);
+    }
+
+    // @Test
+    // public void naoDeveCadastrarQuandoDiferenteDeGmail() {
+		
+    //     // arrange
+    //     String email = "teste@teste.com";
+    //     String senha = "1";
+
+    //     IdentityServiceImpl sut = new IdentityServiceImpl();
+        
+    //     // act
+    //     Usuario usuario = sut.cadastrarUsuario(email, senha);
+
+    //     // assert
+    //     assertNull(usuario);
+    // }
+}
