694ab1691b1a2a174677d38ea51f8b11cd18cb55
Leonardo souza <leonardotreze@gmail.com>
2020-08-24 17:52:04 -0300

    implementacao

--- src/test/java/com/example/demo/ProdutoTests.java
+++ src/test/java/com/example/demo/ProdutoTests.java
@@ -24,14 +24,44 @@ import java.math.BigDecimal;
 @SpringBootTest
 public class ProdutoTests {
 
+    @Mock
+    ProdutoRepository repository;
+
+    @Mock
+    Logger logger;
+
+
     /// eu espero conseguir inserir um produto no repositorio
     @Test
     public void Foo1() {
+        ProdutoRepositoryImpl repository = new ProdutoRepositoryImpl();
+        String nome = "Fulano";
+        BigDecimal preco = new BigDecimal(15.5);
+        
+        Produto produto = repository.inserir(nome, preco);
+
+        assertNotNull(produto);
+        assertEquals("Fulano", produto.getNome());
+        assertEquals( new BigDecimal(15.5), produto.getPreco());
     }
 
     /// eu não devo conseguir inserir um produto quando o preço for maior que 10000
     @Test
     public void Foo2() {
+        String nome = "Ciclano";
+        BigDecimal preco = new BigDecimal(11000);
+        Produto produto = new Produto(nome, preco);
+       
+        
+        when(repository.inserir(Mockito.anyString(), Mockito.any())).thenReturn(produto);
+        
+        // act
+        ProdutoServiceImpl produtoServiceImpl = new ProdutoServiceImpl(repository, logger);
+        Produto produtoRetorno =  produtoServiceImpl.inserirProduto(nome, preco);
+
+        assertNull(produtoRetorno);   
+
+        verify(logger).gravar("Preço fora da faixa permitida");
     }
 
     /// eu devo conseguir inserir um produto e não depender do serviço de log
