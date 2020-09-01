8343f0d61e5327cc6810d20ba9b49b4766c8d43f
Leonardo souza <leonardotreze@gmail.com>
2020-09-01 18:46:40 -0300

    ajustes

--- src/test/java/com/example/demo/ProdutoTests.java
+++ src/test/java/com/example/demo/ProdutoTests.java
@@ -24,7 +24,7 @@ import java.math.BigDecimal;
 @SpringBootTest
 public class ProdutoTests {
 
-    /// eu espero conseguir gravar um produto no repositorio
+    /// eu espero conseguir gravar um produto no ProdutoRepositoryImpl
     @Test
     public void Foo1() {
     }
@@ -34,13 +34,7 @@ public class ProdutoTests {
     public void Foo2() {
     }
 
-    /// eu devo conseguir inserir um produto e não depender do serviço de log
-    @Test
-    public void Foo3() {
-        
-    }
-
-    /// eu preciso saber se o repositório é chamado ao salvar um produto válido
+    /// eu preciso saber se o repositório é chamado ao salvar um produto válido no serviço
     @Test
     public void Foo4() {
 
