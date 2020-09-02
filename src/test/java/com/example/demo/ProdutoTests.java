7f9745908c53cb763f75457a3a2b3012e44dd7b8
Leonardo souza <leonardotreze@gmail.com>
2020-09-02 18:36:22 -0300

    implementacão de testes

--- src/test/java/com/example/demo/ProdutoTests.java
+++ src/test/java/com/example/demo/ProdutoTests.java
@@ -24,19 +24,60 @@ import java.math.BigDecimal;
 @SpringBootTest
 public class ProdutoTests {
 
+    // K.I.S.S.
+    @Mock
+    private Logger logger;
+    @Mock
+    private ProdutoRepository produtoRepository;
     /// eu espero conseguir gravar um produto no ProdutoRepositoryImpl
     @Test
-    public void Foo1() {
+    public void Foo1() throws Exception {
+        // arrange
+        String nomeProduto = "PS5";
+        BigDecimal preco = new BigDecimal(10);
+
+        // act
+        ProdutoRepositoryImpl sut = new ProdutoRepositoryImpl();
+        Produto produto = sut.inserir(nomeProduto, preco);
+
+        // assert
+        assertNotNull(produto);
+        assertEquals(produto.getNome(), nomeProduto);
+        assertEquals(produto.getPreco(), preco);
     }
 
     /// eu não devo conseguir inserir um produto quando o preço for maior que 10000
     @Test
-    public void Foo2() {
+    public void naoDeveInserirProdutoSePrecoMaior10000() throws Exception {
+        // arrange
+        String nomeProduto = "PS5";
+        BigDecimal preco = new BigDecimal(10005);
+
+        ProdutoServiceImpl sut = new ProdutoServiceImpl(produtoRepository, logger);
+
+        //act
+        Produto produto = sut.inserirProduto(nomeProduto, preco);
+
+        //assert 
+        assertNull(produto);
+        verify(logger).gravar("Preço fora da faixa permitida");
     }
 
     /// eu preciso saber se o repositório é chamado ao salvar um produto válido no serviço
     @Test
-    public void Foo4() {
+    public void deveChamarRepositorioAoSalvarUmProdutoValidoNoServico() throws Exception {
+        // arrange
+        String nome = "TV";
+        BigDecimal preco = new BigDecimal(7000);
+        ProdutoServiceImpl sut = new ProdutoServiceImpl(produtoRepository, logger);
 
+        when(produtoRepository.inserir(nome, preco)).thenReturn(new Produto(nome, preco));
+
+        //act
+        Produto produto = sut.inserirProduto(nome, preco);
+
+        //assert
+        assertNotNull(produto);
+        verify(produtoRepository).inserir(nome, preco);
     }
 }
